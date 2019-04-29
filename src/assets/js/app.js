98d38ef0aa249025767d78cc7e13cf7b351e6566
Andrej <hello@andrzej.sk>
2019-04-29 23:57:04 +0200

    add responsive header with menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,11 +7,31 @@ import 'what-input';
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
 window.jQuery = $;
-require('foundation-sites');
+// require('foundation-sites');
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
 
 
 $(document).foundation();
+
+
+
+
+
+$('.site-navigation').on( 'toggled.zf.responsiveToggle', function( e ) {
+	// console.log( 'tooogle' );
+	// console.log( e );
+	let $this = $(this);
+	let $topBar = $this.find('.top-bar');
+	let openClass = 'menu-open';
+
+	if( $topBar.is( ':visible' ) ) {
+		// console.log( 'open' );
+		$this.addClass( openClass );
+	} else {
+		// console.log( 'closed' );
+		$this.removeClass( openClass );
+	}
+});
