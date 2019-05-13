e7c01c4ef2987bdfc94d3a6e8309c95a0fa9a94b
Andrej <hello@andrzej.sk>
2019-05-13 16:32:30 +0200

    feedback (mostly english related), new headers

--- src/assets/vendor/countdown.js
+++ src/assets/vendor/countdown.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function($) {
 	var longWayOff = new Date('August 1, 2019 12:00:00');
+	// var currentLang = $('html').attr('lang');
+	// var layoutString = '';
+
+	// if( currentLang.length && currentLang === 'en' ) {
+	// 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
+	// } else {
+	// 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
+	// }
+
 	$('#countdown').countdown({
 		until: longWayOff, 
 		format: 'odH', 
