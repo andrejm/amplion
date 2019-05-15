128b32f2ff786e4cfaa1441349de396ecea0ac85
Andrej <hello@andrzej.sk>
2019-05-15 23:31:52 +0200

    add fakla replacer script

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,10 +12,39 @@ window.jQuery = $;
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 import './lib/foundation-explicit-pieces';
+// import './lib/fakla_replacer';
 
 
 $(document).foundation();
 
+
+
+// Fakla replacer is script made for Jano Fakla to avoid certain short words in Slovak language to be left alone at the end of line. 
+// updated with english shorts as well 
+// idea https://harrison314.github.io/SlovakWyamTypography.html
+// script https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
+// tested with: https://regex101.com/
+
+jQuery(document).ready(function($) {
+
+	var pagecontent = $( 'p, li' );
+	var result = '';
+	var re = new RegExp('(\\s|^)(z|zo|bez|na|po|od|do|pri|pre|so|miesto|o|v|s|za|a|i|ani|aj|najprv|potom|ešte|ale|no|lež|jednako|alebo|buď|či|že|aby|čo|aký|ktorý|kde|keď|kým|kde|čo|akoby|lebo|pretože|predsa|the|of|at|in)(\\s+)([^\\p{Cc}\\p{Cf}\\p{Z}]+)', 'gi');
+
+	function replaceAll(str, find, replace) {
+	    return str.replace(find, replace);
+	}
+
+	$.each( pagecontent, function(i, iitem){
+		/* console.log( iitem ); */
+	  var $iitem = $(iitem);
+		result = replaceAll($iitem.html(), re, '$1$2&nbsp;$4');
+	  $iitem.html(result);
+	  /* console.log(result); */
+	  
+	} );
+	
+});
 
 
 
