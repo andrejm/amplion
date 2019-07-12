b5043ec087428f5c6e056d89e79164b13dd81a7e
Andrej <hello@andrzej.sk>
2019-07-12 21:19:33 +0200

    compress photos, add photos, add photo authors, change photos... last minute feedback

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -67,4 +67,26 @@ $('.site-navigation').on( 'toggled.zf.responsiveToggle', function( e ) {
 
 jQuery(document).ready(function($) {
 	// var tabs = new Tabby('[data-tabs]');
+
+
+});
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
 });
