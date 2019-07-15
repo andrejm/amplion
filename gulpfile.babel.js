28d76bee3a215b743887a1ef0aa8aa370d38c213
Andrej <hello@andrzej.sk>
2019-07-15 09:32:55 +0200

    change name of photos folder, manage automatic copy of added photos on gulp start

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -32,7 +32,7 @@ function loadConfig() {
 // Build the "dist" folder by running all of the below tasks
 // Sass must be run later so UnCSS can search for used classes in the others assets.
 gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, javascript, images, copy), sass, styleGuide));
+ gulp.series(clean, gulp.parallel(pages, javascript, images, copy, copyGallery), sass, styleGuide));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -49,6 +49,12 @@ function clean(done) {
 function copy() {
   return gulp.src(PATHS.assets)
     .pipe(gulp.dest(PATHS.dist + '/assets'));
+}
+
+// Copy gallery photos
+function copyGallery() {
+  return gulp.src(PATHS.gallery)
+    .pipe(gulp.dest(PATHS.dist + '/gallery'));
 }
 
 // Copy page templates into finished HTML files
@@ -163,6 +169,7 @@ function reload(done) {
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
   gulp.watch(PATHS.assets, copy);
+  gulp.watch(PATHS.gallery, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
