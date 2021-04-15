5960cbb541bd753a293570f92ca68456dcf6fa0a
marosstudenic <studenic.maros@gmail.com>
2021-04-16 01:17:15 +0200

    prve zmeny datum - obrazky - postavicky - chyba homepage v eng

--- src/assets/vendor/countdown.js
+++ src/assets/vendor/countdown.js
@@ -1,16 +1,18 @@
-jQuery(document).ready(function($) {
-	var longWayOff = new Date('September 25, 2020 16:00:00');
-	// var currentLang = $('html').attr('lang');
-	// var layoutString = '';
+jQuery(document).ready(function ($) {
+  var longWayOff = new Date("September 24, 2021 16:00:00");
+  // var currentLang = $('html').attr('lang');
+  // var layoutString = '';
 
-	// if( currentLang.length && currentLang === 'en' ) {
-	// 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
-	// } else {
-	// 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
-	// }
+  // if( currentLang.length && currentLang === 'en' ) {
+  // 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
+  // } else {
+  // 	layoutString = '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>';
+  // }
 
-	$('#countdown').countdown({
-		until: longWayOff, 
-		format: 'odH', 
-		layout: '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>'});
+  $("#countdown").countdown({
+    until: longWayOff,
+    format: "odH",
+    layout:
+      '<div class="counter__item">{on}<span class="counter__label">{ol}</span></div> <div class="counter__item">{dn}<span class="counter__label">{dl}</span></div> <div class="counter__item">{hn}<span class="counter__label">{hl}</span></div>',
+  });
 });
